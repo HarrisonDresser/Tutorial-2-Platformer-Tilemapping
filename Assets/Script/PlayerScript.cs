aff7879ae371579ddb2abb5552d21704e6bbd960
HarrisonDresser <harry.dresser777@gmail.com>
2021-10-24 23:15:34 -0400

    Challenge commit

--- Assets/Script/PlayerScript.cs
+++ Assets/Script/PlayerScript.cs
@@ -2,19 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro; 
 
 public class PlayerScript : MonoBehaviour
 {
     private Rigidbody2D rd2d;
     public float speed;
+
+    public AudioSource playerAudioSource;
+
     public Text score;
-    private int scoreValue = 0;
+    public int scoreValue = 0;
+
+    public Text lives;
+    private int livesValue = 3;
+
+    public int R2 = 0;
+
+    public GameObject winTextObject;
+    public GameObject loseTextObject;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
+
+        lives.text = "lives: " + livesValue.ToString();
+
         score.text = scoreValue.ToString();
+
+        winTextObject.SetActive(false);
+        loseTextObject.SetActive(false);
 
     }
 
@@ -25,6 +45,7 @@ public class PlayerScript : MonoBehaviour
         float verMovement = Input.GetAxis("Vertical");
         rd2d.AddForce(new Vector2( hozMovement * speed, verMovement * speed));
         
+
         if (Input.GetKey("escape"))
         {
             Application.Quit();
@@ -33,11 +54,36 @@ public class PlayerScript : MonoBehaviour
     
     private void OnCollisionEnter2D(Collision2D collision)
     {
-       if (collision.collider.tag == "Coin")
+        GameObject ThePlayer = GameObject.Find("Player 2"); 
+
+        if (collision.collider.tag == "Coin")
         {
             scoreValue += 1;
             score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+        }
+        if (collision.collider.tag == "Enemy")
+        {
+            livesValue -= 1;
+            lives.text = "lives: " + livesValue.ToString();
+            Destroy(collision.collider.gameObject);
+        }
+        if (livesValue <= 0)
+        {
+            Destroy(ThePlayer);
+            loseTextObject.SetActive(true);
+        }
+
+        if (scoreValue >= 4 && R2 == 0)
+        {
+            transform.position = new Vector3(85.0f, 1.0f, 0.0f); 
+            R2 = 1;
+            livesValue = 3;
+            lives.text = "lives: " + livesValue.ToString();
+        }
+        if (scoreValue >= 8)
+        {
+            winTextObject.SetActive(true);
         }
 
     }
---
+++ Assets/Script/SoundScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundScript : MonoBehaviour
+{
+    public AudioSource musicSource;
+
+    public AudioSource playerAudio;
+
+    public AudioClip musicClipOne;
+
+    public AudioClip musicClipTwo;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
